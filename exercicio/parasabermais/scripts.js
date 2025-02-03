53757faef7c3cd18182f7f9d30bdd71fa693330b
David T. <davidt.info@gmail.com>
2025-02-03 17:01:17 -0300

    Envio do formulário

--- exercicio/parasabermais/scripts.js
+++ exercicio/parasabermais/scripts.js
@@ -1,22 +1,9 @@
-document.getElementById('imageInput').addEventListener('change', function(event) {
-    const file = event.target.files[0]; // Pegando o arquivo selecionado pelo usuário
-    if (file) {
-        const reader = new FileReader(); // Criando uma instância do FileReader
-        reader.onload = function(e) {
-            const preview = document.getElementById('preview');
-            preview.src = e.target.result; // Atribuindo o resultado da leitura como fonte da imagem de pré-visualização
-            preview.style.display = 'block'; // Tornando a pré-visualização visível
-        };
-        reader.readAsDataURL(file); // Lendo o arquivo como um Data URL
+let promessaDePizza = new Promise(function(resolve, reject) {
+    // Simulando a entrega da pizza
+    let pizzaEntregue = true; // Tente mudar para false e veja o que acontece
+    if (pizzaEntregue) {
+      resolve('Pizza entregue com sucesso!');
+    } else {
+      reject('Entrega da pizza falhou.');
     }
-});
-
-inputUpload.addEventListener("change", (evento) => {
-    const arquivo = evento.target.files[0];
-
-    if (arquivo) {
-        try {
-            const conteudoDoArquivo = lerConteudoDoArquivo(arquivo);
-        }
-    }
-})
+  });
--- scripts.js
+++ scripts.js
@@ -84,28 +84,31 @@ inputTags.addEventListener("keypress", async (evento) => {
 
 const botaoPublicar = document.querySelector(".botao-publicar");
 
-botaoPublicar.addEventListener("click", async (evento) => {
-    evento.preventDefault();
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value;
-    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
-
-    console.log(nomeDoProjeto)
-    console.log(descricaoDoProjeto)
-    console.log(tagsProjeto)
-
-})
-
-async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
-    return new Promisse((resolve, reject) => {
+async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             const deuCerto = Math.random() > 0.5;
-
             if (deuCerto) {
-                resolve("Projeto publicado com sucesso")
+                resolve("Projeto publicado com sucesso.")
             } else {
                 reject("Erro ao publicar o projeto.")
             }
         }, 2000)
     })
 }
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo!");
+    } catch (error) {
+        console.log("Deu errado: ", error);
+        alert("Deu tudo errado!");
+    }
+})
